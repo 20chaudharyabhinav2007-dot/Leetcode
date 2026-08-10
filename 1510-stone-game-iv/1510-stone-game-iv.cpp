ba6c0b2e9a1581a0c3f872fca47b1e5a6bd64f01
Abhinav Chaudhary <20chaudharyabhinav2007@gmail.com>
2026-08-10 20:08:09 +0530

    Time: 39 ms (67.82%), Space: 14.1 MB (54.42%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[100001];
+
+    bool winnerSquareGame(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
+    }
+
+    bool solve(int n) {
+        if(n == 0)
+            return false;
+
+        if(dp[n] != -1)
+            return dp[n];
+
+        for(int i = 1; i * i <= n; i++) {
+            if(solve(n - i * i) == false) {
+                return dp[n] = true;
+            }
+        }
+
+        return dp[n] = false;
+    }
+};
