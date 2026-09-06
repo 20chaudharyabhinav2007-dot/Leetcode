021440dd36761c1b6ebca5b720b7ee04053239bf
Abhinav Chaudhary <20chaudharyabhinav2007@gmail.com>
2026-09-06 09:34:56 +0530

    Time: 7 ms (36.86%), Space: 103.2 MB (66.49%) - LeetHub

---
+++ 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countGoodRotations(vector<int>& nums) {
+        int n = nums.size();
+        int h = n/2;
+        long long total = 0;
+        for( int x : nums){
+            total+= x;
+        }
+        long long firsum = 0;
+        for( int i = 0; i<h ; i++){
+            firsum+= nums[i];
+        }
+        long long ans = 0;
+        for( int i=0 ; i<n ; i++){
+            if(2*firsum > total){
+                ans++;
+            }
+            firsum -= nums[i];
+            firsum += nums[(h+i)%n];
+        }
+        return ans;
+    }
+};
