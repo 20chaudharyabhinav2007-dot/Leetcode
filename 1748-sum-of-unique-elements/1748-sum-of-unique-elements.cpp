b514e806468050a2af510075b3c68b7ebca9bcad
Abhinav Chaudhary <20chaudharyabhinav2007@gmail.com>
2026-09-19 22:33:21 +0530

    Time: 0 ms (100%), Space: 10.6 MB (63.46%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int freq[101] = {0};
+        int sum = 0;
+        for (int x : nums) {
+            freq[x]++;
+        }
+        for (int x : nums) {
+            if (freq[x] == 1) {
+                sum += x;
+            }
+        }
+        return sum;
+    }
+};
